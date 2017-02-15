6ef52cd6f505d23bb4d5cefabe69df47fb71ef62
EmoBurrito <smith7351@saskpolytech.ca>
2017-02-14 19:22:40 -0600

    Accidentally added a bunch of junk information. Recomitting

--- DebugA1/doxygen/html/files.js
+++
@@ -1,6 +0,0 @@
-var files =
-[
-    [ "q1.h", "q1_8h_source.html", null ],
-    [ "q2.h", "q2_8h_source.html", null ],
-    [ "q3.h", "q3_8h_source.html", null ]
-];
--- DebugA1/doxygen/html/navtreeindex0.js
+++
@@ -1,9 +0,0 @@
-var NAVTREEINDEX0 =
-{
-"files.html":[0,0],
-"index.html":[],
-"pages.html":[],
-"q1_8h_source.html":[0,0,0],
-"q2_8h_source.html":[0,0,1],
-"q3_8h_source.html":[0,0,2]
-};
