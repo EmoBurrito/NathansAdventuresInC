a353a86c32b300ca8cc485b1e1bdb5995bc954b0
Nathan Smith <smith7351@saskpolytech.ca>
2017-04-17 21:35:41 -0600

    Tried

--- ExamPractice/program.c
+++ ExamPractice/program.c
@@ -12,13 +12,19 @@
 //So, if you pass in a char set of 0x32, it will return a char set of 0x23 
 unsigned char swapNibbles(unsigned char c)
 {
-	return 0;
+	return (c << 4) + (c >> 4);
 }
 
 //Write a function that takes an integer (8 nibbles) and displays the value of each nibble on a separate line
 void displayNibbles(unsigned int iValue)
 {
-	//Nathan thinks this may be easier to break into chars, then nibbles but Nathan also thinks he's best gurl
+	unsigned int mask = 0xC000; //1100 0000 0000 0000 in binary
+
+	for (int i = 0; i < 8; ++i) //Magic numbers AND system specific... Rob'll have my hide
+	{
+		printf("%d: %x\n", i, iValue & mask);
+		mask = mask >> 2;
+	}
 }
 
 //Write a function that takes two chars, a and b and combines the data as follows, returning char c
@@ -28,12 +34,13 @@ char mashBits(unsigned char a, unsigned char b)
 	//char a = XX!! YY!!
 	//char b = !!ZZ ZZ!!
 	//return ZZZZ XXYY 
-	return 0;
+	return (a & 0xC0) + (a & 0x0C) + (b & 0x3C);
 }
 
 int main (void)
 {
 	printf("<Swap Nibbles> Expected: %i. Result: %i\n", 0x23, swapNibbles(0x32));
-	printf("<displayNibbles>");
-	printf("<Mash Bits> Expected: %c. Result: %c\n", 'a', 'b');
+	printf("<displayNibbles>Uhh...\n");
+	displayNibbles(27);
+	printf("<Mash Bits> Expected: %i. Result: %i\n", 0xC8, mashBits(0xB3, 0x33));
 }
